fea2c35ca887f3f4fd2817e786a7c704154830b9
rajag <rajagurru333@gmail.com>
2024-01-06 13:15:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ContentPage from './components/content';
+import HeaderPage from './components/header'
+function App() {
+  return (
+    <div className="App">
+      <HeaderPage />
+      <ContentPage/>
+      </div>
+  );
+}
+
+export default App;
